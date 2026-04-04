4a56b85d068ce38951b5ba418a4b9462046763bb
JOMinSeon <fjkg33@naver.com>
2026-04-04 16:32:42 +0900

    Update kakao map height to 600px

--- components/HospitalMap.tsx
+++ components/HospitalMap.tsx
@@ -231,7 +231,7 @@ export function HospitalMap({
       <div
         ref={mapRef}
         className={`rounded-xl overflow-hidden ${className}`}
-        style={{ height: '400px', minHeight: '400px' }}
+        style={{ height: '600px', minHeight: '600px' }}
       />
       {!mapLoaded && (
         <div className="absolute inset-0 flex items-center justify-center rounded-xl"
